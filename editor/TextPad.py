e8f425edebe6df180c110d5f7a6f78adda33706b
pendragonzo <hi@pendragonzo.org>
2020-10-02 16:13:28 +0100

    Adding ConfigSettings.

--- editor/TextPad.py
+++ editor/TextPad.py
@@ -1,18 +1,22 @@
 from .Graphics import Tkinter
+from .ConfigSettings import Connect
 
 class TextPad(Tkinter.Text):
 	def __init__(self, *args, **kwargs):
 		Tkinter.Text.__init__(self, *args, **kwargs)
+		self.storeobj = {}
+		self.Connect_External_Module_Features()
 		self._pack()
 
+	def Connect_External_Module_Features(self):
+		Connect(self)
+		return
 
 	def _pack(self):
 		self.pack(expand = True, fill = "both")
 		return
 
-
-
 if __name__ == '__main__':
-	root = Tkinter.Tk(className = " Test TextPad")
-	TextPad(root)
-	root.mainloop()
+		root = Tkinter.Tk(className = " Test TextPad")
+		TextPad(root)
+		root.mainloop()
